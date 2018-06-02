8487e96fc690be95731b13b5755b572f4c89ce2f
DAWdFarras <38564592+DAWdFarras@users.noreply.github.com>
2018-06-02 19:16:49 +0200

    Update home.js

--- js/home.js
+++ js/home.js
@@ -9,57 +9,45 @@ var contador = 0;
 var contadorTotal = 0;
 var autoRef = false;
 
-/* Función Windows onload */
 $(function () {
+    $('#loadButton').click(function () {
+        cargarMas();
+    });
+    $('#autoRefresh').click(function () {
+        autoRef=true
+    });
+    $(window).scroll(function () {
+        cargarScroll();
+    });
+});
 
-$('#loadButton').click(function () {
-   cargarMas();
-});
-$('#autoRefresh').click(autoRef=true);
-$(window).scroll(function () {
-    cargarScroll();
-});
-});
-
-/**
-* Función para capturar el evento on click del boton "LOAD MORE NEWS"
-*/
 function cargarMas() {
-
-if (contadorTotal <= 10) {
-    if (contador < jsonList.length) {
-        $.getJSON(jsonList[contador], function (jsonObject) {
+   if (contadorTotal <= 10) {
+      if (contador < jsonList.length) {
+         $.getJSON(jsonList[contador], function (jsonObject) {
             pintar(jsonObject);
-        });
-
-        
-        contador++;
-        contadorTotal++
-        if (contador == 3) {
+         });
+         contador++;
+         contadorTotal++
+         if (contador == 3) {
             contador = 0
-        };
-    };
-}else{
-    alert("There isn´t more news.")
-};
+         };
+      };
+   }else{
+      alert("There isn´t more news.")
+   };
 };
 
-/* Funcion para cargar el json con los contenedores dentro de una variable y luego introducirla en la página web*/
 function pintar(json) {
-datos="";
-$.each(json.news, function (i, news) {
-    datos += "<div class='row secondaryNew'><div class='col-sm-5'><img class='img-responsive img-rounded' src='"+news.img+"' /></div><div class='col-sm-7'><div class='row'><h1>"+news.title+"</h1></div><div class='row'><div class='col-sm-12 bg-light' id='newsText'>"+news.description+"</div></div><div class='row'><div class='col-sm-12 bg-light' id='links'>"+news.date+"</div></div></div></div>";
-});                                                                                                                                                                   
-$('#someMoreNews').append(datos);
+   datos="";
+      $.each(json.news, function (i, news) {
+         datos += "<div class='row secondaryNew'><div class='col-sm-5'><img class='img-responsive img-rounded' src='"+news.img+"' /></div><div class='col-sm-7'><div class='row'><h1>"+news.title+"</h1></div><div class='row'><div class='col-sm-12 bg-light' id='newsText'>"+news.description+"</div></div><div class='row'><div class='col-sm-12 bg-light' id='links'>"+news.date+"</div></div></div></div>";
+      });                                                                                                                                                                   
+   $('#someMoreNews').append(datos);
 }
 
-/**
-* Función para controlar si el scroll esta a menos de 100 pixeles del final de la pantalla, y si tiene seleccionado la opcion "cargar con Scroll"
-*/
 function cargarScroll() {
-
     if (($(window).scrollTop() + $(window).height() > $(document).height() - 100) && autoRef ) {
         cargarMas();
     };
-
 };
